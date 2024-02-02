84b7265d6c30aadaf68285237cdab50d4dac8909
Sarthak Agrawal <sarthakagrawal927@gmail.com>
2024-02-02 21:51:36 +0530

    Add sectoral grouping

--- app.js
+++ app.js
@@ -22,24 +22,69 @@ try {
 }
 
 result = result
-  .filter(
-    (r) =>
-      typeof r["Close_Price"] === "number" &&
-      typeof r["Market_Cap"] === "number" &&
-      typeof r["EverythingAllAtOnce"] === "number" &&
-      typeof r["Overbought_100"] === "number",
-  )
   .map((r) => ({
     Name: r["Name"],
-    Sub_Sector: r["Sub_Sector"],
-    Close_Price: r["Close_Price"],
-    Market_Cap: r["Market_Cap"],
-    Score: r["EverythingAllAtOnce"],
-    OverBought: 100 - r["Overbought_100"],
-    Change_6m: r["change_6m"],
+    Sub_Sector: r["Sub-Sector"],
+    Market_Cap: r["Market Cap"],
+    Close_Price: r["Close Price"],
+    // Score: r["EverythingAllAtOnce"],
+    // OverBought: 100 - r["Overbought_100"],
+    // Change_6m: r["change_6m"],
     OneYearReturn: r["1Y Return"],
     FiveYearCagr: r["5Y CAGR"],
-  }));
+  }))
+  .filter(
+    (r) =>
+      typeof r.Market_Cap === "number" &&
+      typeof r.Close_Price === "number" &&
+      // typeof r["EverythingAllAtOnce"] === "number" &&
+      // typeof r["Overbought_100"] === "number" &&
+      typeof r.OneYearReturn === "number" &&
+      typeof r.FiveYearCagr === "number" &&
+      r.Sub_Sector !== undefined,
+  );
+
+console.log({ result });
+console.log({ result: result.length });
+
+const sectorGrouped = result.reduce((acc, curr) => {
+  if (!acc[curr.Sub_Sector]) {
+    acc[curr.Sub_Sector] = {
+      count: 0,
+      marketCap: 0,
+      sumOneYearReturn: 0,
+      sumFiveYearCagr: 0,
+    };
+  }
+  acc[curr.Sub_Sector].count++;
+  acc[curr.Sub_Sector].marketCap += curr.Market_Cap;
+  acc[curr.Sub_Sector].sumOneYearReturn += curr.OneYearReturn;
+  acc[curr.Sub_Sector].sumFiveYearCagr += curr.FiveYearCagr;
+  return acc;
+}, {});
+
+const sectoredGroupArray = Object.keys(sectorGrouped)
+  .map((key) => ({
+    Sub_Sector: key,
+    count: sectorGrouped[key].count,
+    avgMarketCap: sectorGrouped[key].marketCap / sectorGrouped[key].count,
+    // avgScore: sectorGrouped[key].sumScore / sectorGrouped[key].count,
+    avgOneYearReturn:
+      sectorGrouped[key].sumOneYearReturn / sectorGrouped[key].count,
+    avgFiveYearCagr:
+      sectorGrouped[key].sumFiveYearCagr / sectorGrouped[key].count,
+  }))
+  .sort((a, b) => b.avgOneYearReturn - a.avgOneYearReturn);
+
+sectoredGroupArray.map((s) => {
+  console.log(
+    s.Sub_Sector,
+    s.avgMarketCap,
+    s.count,
+    s.avgOneYearReturn,
+    s.avgFiveYearCagr,
+  );
+});
 
 let sumClosePrice = 0;
 let sumMarketCap = 0;
@@ -53,10 +98,10 @@ for (let i = 0; i < result.length; i++) {
   sumOverBought += result[i]["OverBought"];
 }
 
-console.log({
-  close: sumClosePrice / result.length,
-  mktCap: sumMarketCap / result.length,
-  score: sumScore / result.length,
-  overBought: sumOverBought / result.length,
-  len: result.length,
-});
+// console.log({
+//   close: sumClosePrice / result.length,
+//   mktCap: sumMarketCap / result.length,
+//   score: sumScore / result.length,
+//   overBought: sumOverBought / result.length,
+//   len: result.length,
+// });
